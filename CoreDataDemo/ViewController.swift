be9ce47e4fd88ba9d396097f0676f077695704b3
teaberry73 <74955723+teaberry73@users.noreply.github.com>
2022-03-06 15:06:24 -0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Regina Williams on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
