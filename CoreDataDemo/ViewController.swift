8405d7b5b9d129f20366491e7515c880c49a9954
teaberry73 <74955723+teaberry73@users.noreply.github.com>
2022-03-06 16:07:55 -0500

    new project with core data

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //  used viewContext to update/retrieve data)
+        //  how to retrieve the managed object context througout the project:
+       //  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     }
 
 
---
+++ Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  CoreDataDemo
+//
+//  Created by Regina Williams on 3/6/22.
+//
+
+import Foundation
+import CoreData
+
+class Person: NSManagedObject {
+    
+}
